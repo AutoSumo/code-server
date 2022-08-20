72617cbce85a822dcdf517357fe78efac659e224
techchrism <chris@techchrism.me>
2022-08-20 15:54:18 -0700

    add websockets for highlighting

--- index.js
+++ index.js
@@ -5,9 +5,67 @@ const koaRespond = require('koa-respond');
 const Router = require('koa-router');
 const cors = require('@koa/cors');
 const sanitize = require('sanitize-filename');
+const websockify = require('koa-websocket');
 const fs = require('fs').promises;
+require('dotenv').config();
 
-const app = new Koa();
+const app = websockify(new Koa());
+const robotServerID = process.env['ROBOT_SERVER_ID'];
+
+
+let clients = [];
+app.ws.use((ctx, next) =>
+{
+    ctx.websocket.once('message', message =>
+    {
+        try
+        {
+            const data = JSON.parse(message.toString());
+            if(data.type === 'register' && !!data.id)
+            {
+                console.log(`${data.id} vs ${robotServerID}`);
+                if(data.id === robotServerID)
+                {
+                    ctx.websocket.on('message', message =>
+                    {
+                        const data = JSON.parse(message.toString());
+                        if(data.type === 'highlight')
+                        {
+                            const codeID = data.codeID;
+                            for(const client of clients)
+                            {
+                                if(client.id === codeID)
+                                {
+                                    client.ws.send(JSON.stringify({
+                                        type: 'highlight',
+                                        statement: data.statement
+                                    }));
+                                }
+                            }
+                        }
+                    });
+                }
+                else
+                {
+                    clients.push({id: data.id, ws: ctx.websocket});
+                }
+            }
+            else
+            {
+                ctx.websocket.close();
+            }
+        }
+        catch(e)
+        {
+            ctx.websocket.close();
+        }
+    });
+
+    ctx.websocket.on('close', () =>
+    {
+        clients = clients.filter(c => c.ws !== ctx.websocket);
+    });
+});
 
 app.use(logger());
 app.use(koaBody());
